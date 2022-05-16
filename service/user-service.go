01b24321d4b8029c9ab20ca260fff37d7f3538c5
zanuar35 <rahmatzanuar35@gmail.com>
2022-05-16 12:10:17 +0700

    add user service

---
+++ service/user-service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"log"
+
+	"github.com/mashingan/smapping"
+	"gorm.io/dto"
+	"gorm.io/entity"
+	"gorm.io/repository"
+)
+
+type UserService interface {
+	Update(user dto.UserUpdateDTO) entity.User
+	Profile(userID string) entity.User
+}
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+func NewUserService(userRepo repository.UserRepository) UserService{
+	return &userService{
+		userRepository: userRepo,
+	}
+}
+
+func (service *userService) Update(user dto.UserUpdateDTO) entity.User{
+	userToUpdate := entity.User{}
+	err := smapping.FillStruct(&userToUpdate, smapping.MapFields(&user))
+	if err != nil{
+		log.Fatalf("failed map %v", err)
+	}
+	updateUser := service.userRepository.UpdateUser(userToUpdate)
+	return updateUser
+}
+
+func (service *userService) Profile(userID string) entity.User{
+	return service.userRepository.ProfileUser(userID)
+}
